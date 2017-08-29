145937bfe3d792dec4c813fdd8ac80417b413fb8
张泽宇 <zhangzeyu@lztogether.com>
2017-08-29 17:11:25 +0800

    Add 增加file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+    entry : './src/index.js',
+    output : {
+        filename : 'bundle.js',
+        path : path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [{test:/\.css$/,use: ['style-loader','css-loader']}]
+    module : {
+        rules : [
+            {test : /\.css$/, use : ['style-loader', 'css-loader']},
+            {test : /\.(png|svg|jpg|gif)$/, use : ['file-loader']}
+        ]
 
     }
 };
